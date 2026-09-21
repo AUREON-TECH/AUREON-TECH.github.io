59f4b53cdc4778cb060e3c81e22b47d94a832016
raphaelbuenocaptacao-creator <raphaelbueno.captacao@gmail.com>
2026-09-21 13:13:32 -0300

    fix: evitar JavaScript obsoleto no cache do PWA

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE = "aureon-diagnostico-v3";
+const CACHE = "aureon-diagnostico-v4";
 const SHELL = [
   "./", "./index.html", "./css/diagnostico.css", "./icon.svg",
   "./icons/icon-192.png", "./icons/icon-512.png", "./icons/icon-maskable-512.png",
   "./js/site-config.js", "./js/questions.js", "./js/engine.js", "./js/state.js",
-  "./js/whatsapp.js", "./js/app.js", "./terms.html", "./privacy.html",
+  "./js/whatsapp.js", "./js/app.js?v=20260921-4", "./terms.html", "./privacy.html",
   "./manifest.webmanifest"
 ];
 
@@ -31,6 +31,14 @@ self.addEventListener("fetch", (event) => {
       caches.open(CACHE).then((cache) => cache.put("./index.html", copy));
       return response;
     }).catch(() => caches.match("./index.html")));
+    return;
+  }
+
+  if (["script", "style"].includes(request.destination)) {
+    event.respondWith(fetch(request).then((response) => {
+      if (response.ok) caches.open(CACHE).then((cache) => cache.put(request, response.clone()));
+      return response;
+    }).catch(() => caches.match(request)));
     return;
   }
 
